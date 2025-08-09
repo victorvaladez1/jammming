efdc51a1c9004f7b7bb3f85a2078eb4ae6997043
Victor Valadez <vvaladez315@gmail.com>
2025-08-09 13:20:30 -0500

    feat: add Playlist component

--- frontend/src/components/Playlist/Playlist.jsx
+++ frontend/src/components/Playlist/Playlist.jsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from "react";
+import TrackList from "./TrackList/TrackList.jsx";
 
-const Playlist = () => {
-
-    
-
+const Playlist = ({playlistName, tracks, onNameChange, onRemove, onSave }) => {
     return (
-        <div>
-
+        <div className="bg-white bg-opacity-10 p-6 rounded-lg shadow-lg w-full max-w-md text-white">
+            <input
+                className="w-full p-2 mb-4 text-black rounded"
+                value={playlistName}
+                onChange={(e) => onNameChange(e.target.value)}
+                placeholder="New Playlist Name"
+            />
+            <TrackList tracks={tracks} onRemove={onRemove} isRemoval={true} />
+            <button 
+                className="mt-6 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded"
+                onClick={onSave}
+            >
+                Save to Spotify
+            </button>
         </div>
     );
 }
